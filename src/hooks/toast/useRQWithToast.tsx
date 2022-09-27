ff0b3a2c70b563b39964f12a9c0eb4185905dbb6
Theodorus Clarence <theodorusclarence@gmail.com>
2022-09-27 11:30:46 +0700

    chore: init rhf, rq, dialog, nprogress

---
+++ src/hooks/toast/useRQWithToast.tsx
@@ -0,0 +1,62 @@
+import { UseQueryResult } from '@tanstack/react-query';
+import * as React from 'react';
+import toast from 'react-hot-toast';
+
+import { DEFAULT_TOAST_MESSAGE } from '@/constant/toast';
+
+type OptionType = {
+  runCondition?: boolean;
+  loading?: string;
+  success?: string;
+  error?: string;
+};
+
+export default function useRQWithToast<T, E>(
+  query: UseQueryResult<T, E>,
+  { runCondition = true, ...customMessages }: OptionType = {}
+) {
+  const { data, isError, error, isLoading } = query;
+
+  const toastStatus = React.useRef<string>(data ? 'done' : 'idle');
+  const toastMessage = {
+    ...DEFAULT_TOAST_MESSAGE,
+    ...customMessages,
+  };
+
+  React.useEffect(() => {
+    if (!runCondition) return;
+
+    // If it is not the first render
+    if (toastStatus.current === 'done' && !isLoading) return;
+
+    if (isError) {
+      if (typeof toastMessage.error === 'string') {
+        toast.error(toastMessage.error, { id: toastStatus.current });
+      } else {
+        toast.error(toastMessage.error(error), { id: toastStatus.current });
+      }
+      toastStatus.current = 'done';
+    } else if (isLoading) {
+      toastStatus.current = toast.loading(toastMessage.loading);
+    } else if (data) {
+      toast.success(toastMessage.success, { id: toastStatus.current });
+      toastStatus.current = 'done';
+    }
+
+    return () => {
+      toast.dismiss(toastStatus.current);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    data,
+    error,
+    isError,
+    isLoading,
+    runCondition,
+    toastMessage.error,
+    toastMessage.loading,
+    toastMessage.success,
+  ]);
+
+  return { ...query };
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,45 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  QueryOptions,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppProps } from 'next/app';
+import Router from 'next/router';
+import nProgress from 'nprogress';
 
 import '@/styles/globals.css';
+import '@/styles/nprogress.css';
 
-/**
- * !STARTERCONF info
- * ? `Layout` component is called in every page using `np` snippets. If you have consistent layout across all page, you can add it here too
- */
+import api from '@/lib/axios';
+
+import DismissableToast from '@/components/DismissableToast';
+
+const defaultQueryFn = async ({ queryKey }: QueryOptions) => {
+  const { data } = await api.get(`${queryKey?.[0]}`);
+  return data;
+};
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: defaultQueryFn,
+    },
+  },
+});
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <DismissableToast />
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
